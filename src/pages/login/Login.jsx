70acbf3bbf7538d47344665029795f5126cb5b5d
tuhinm79 <tuhinmohapatra79@gmail.com>
2024-05-02 21:29:00 +0530

    reg and login validation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,22 +2,56 @@ import React, { useState } from "react";
 import "./Login.css";
 import newRequest from "../../utils/newRequest";
 import { useNavigate,Link } from "react-router-dom";
+import swal from 'sweetalert';
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const res = await newRequest.post("/auth/login", { username, password });
-      localStorage.setItem("currentUser", JSON.stringify(res.data));
-      navigate("/")
-    } catch (err) {
-      setError(err.response.data);
+    if(username===""){
+      swal({
+        title: "Empty Email",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again", 
+      })
+    }
+    // else if(!emailRegex.test(username)){
+    //   swal({
+    //     title: "Not a Valid Email ID",
+    //     // text: "Correct OTP!",
+    //     icon: "error",
+    //     button: "Try Again", 
+    //   })
+    // }
+    else if(password===""){
+      swal({
+        title: "Empty Passwaord",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again", 
+      })
+    }
+    else{
+      try {
+        const res = await newRequest.post("/auth/login", { username, password });
+        localStorage.setItem("currentUser", JSON.stringify(res.data));
+        navigate("/")
+      } catch (err) {
+        // console.log(err.response.data);
+        swal({
+          title: err.response.data,
+          // text: "Correct OTP!",
+          icon: "error",
+          button: "Try Again", 
+        })
+      }
     }
   };
 
@@ -25,7 +59,7 @@ function Login() {
     <div className="login">
       <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
-        <label htmlFor="">Username</label>
+        <label htmlFor="">Email ID</label>
         <input
           name="username"
           type="text"
@@ -39,7 +73,7 @@ function Login() {
           type="password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <button className="loginbuttononhover" type="submit">Login</button>
         {/* {error && error} */}
         <div style={{textAlign:"center"}}><Link to="/register" ><h4>Don't have an account click here</h4></Link></div>
       </form>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,52 +3,82 @@ import upload from "../../utils/upload";
 import "./Register.css";
 import newRequest from "../../utils/newRequest";
 import { useNavigate, Link } from "react-router-dom";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 function Register() {
   const [file, setFile] = useState(null);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   const [user, setUser] = useState({
     username: "",
     email: "",
     password: "",
     img: "",
     country: "",
+    phone: "",
     isSeller: false,
     desc: "",
     inpotp: "",
-    otp:""
+    otp: "",
+    otpvd: false,
   });
 
   // const [otp, setOtp] = useState("");
+  /////////////////////////////
+
+  // const validateform=()=>{
+  //   if(username)
+  // }
 
   const generateOTP = () => {
-    const newOTP = Math.floor(1000 + Math.random() * 9000);
-    user.otp=newOTP.toString();
-    sendoptinemail();
-    
+    // console.log(user.otpvd);
+    if (user.email === "") {
+      swal({
+        title: "Enter Your Email ID",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "OK",
+      });
+    } else if (!emailRegex.test(user.email)) {
+      swal({
+        title: "Not a Valid Email ID",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "OK",
+      });
+    } else {
+      const newOTP = Math.floor(1000 + Math.random() * 9000);
+      user.otp = newOTP.toString();
+      sendoptinemail();
+    }
   };
   const verifyOTP = () => {
     // console.log(user.otp)
     // console.log(user.otp,"hello",user.inpotp)
-    if(user.otp===user.inpotp){
+    // console.log(user.otpvd);
+    if (user.otp === user.inpotp) {
+      setUser((prev) => {
+        return { ...prev, otpvd: true };
+      });
+      // console.log(user.otpvd);
+      // otpvd=true;
       swal({
         title: "OTP Verified",
         // text: "Correct OTP!",
         icon: "success",
-        button: "OK", 
-      })
-    }
-    else{
+        button: "OK",
+      });
+    } else {
       swal({
         title: "Wrong OTP",
         // text: "Correct OTP!",
         icon: "error",
-        button: "OK", 
-      })
+        button: "OK",
+      });
     }
   };
   const sendoptinemail = async () => {
-    console.log(user.otp)
+    console.log(user.otp);
     try {
       await newRequest.post("/email/", {
         otp: user.otp,
@@ -73,6 +103,78 @@ function Register() {
       return { ...prev, isSeller: e.target.checked };
     });
   };
+
+  const registervalidation = async (e) => {
+    e.preventDefault();
+    if (user.username === "") {
+      swal({
+        title: "Empty User Name",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (user.email === "") {
+      swal({
+        title: "Not a Valid Email ID",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (!emailRegex.test(user.email)) {
+      swal({
+        title: "Not a Valid Email ID",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    }else if (!passRegex.test(user.password)) {
+      swal({
+        title: "Create a Strong Password",
+        // text: "It should have a capital le",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (user.password === "") {
+      swal({
+        title: "Empty Password",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (user.country === "") {
+      swal({
+        title: "Empty Country",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (user.phone === "") {
+      swal({
+        title: "Empty Phone Number",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    }  else if (user.phone.length<10) {
+      swal({
+        title: "Not a valid Phone Number",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else if (user.otpvd === false) {
+      swal({
+        title: "OTP Not Verified!",
+        // text: "Correct OTP!",
+        icon: "error",
+        button: "Try Again",
+      });
+    } else {
+      console.log("object")
+      handleSubmit(e);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -82,149 +184,174 @@ function Register() {
         ...user,
         img: url,
       });
-      navigate("/");
+      navigate("/login");
     } catch (err) {
-      console.log(err);
+      // console.log(err);
+      swal({
+          title: err.response.data,
+          // text: "Correct OTP!",
+          icon: "error",
+          button: "Try Again",
+        })
     }
   };
   return (
     <div className="reg">
-      <div style={{textAlign:"center", paddingTop:"20px"}}><h1>Create a new account</h1></div>
-      
-    <div className="register">
-      <form onSubmit={handleSubmit}>
-        
-        <div className="left">
-          {/* <h1>Create a new account</h1> */}
-          <label htmlFor="">Username</label>
-          <input
-            name="username"
-            type="text"
-            placeholder="johndoe"
-            onChange={handleChange}
-          />
-          <label htmlFor="">Email </label>
-          <div
-            className=""
-            style={{
-              display: "flex",
-              gap: "10px",
-              // justifyContent: "center",
-              // alignItems: "center",
-            }}
-          >
-            <div style={{ width: "70%" }}>
-              <input
-                name="email"
-                type="email"
-                placeholder="Email"
-                onChange={handleChange}
-                style={{ width: "80%" }}
-              />
-            </div>
-            <div style={{ width: "30%" }}>
-              <button
-                onClick={generateOTP}
-                className="otpButton"
-                style={{
-                  width: "100%",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  marginTop:"10px",
-                  color: "#fff",
-                  cursor: "pointer",
-                }}
-              >
-                Send OTP
-              </button>
-            </div>
-          </div>
-          {/*  */}
-          <label htmlFor="">Enter OTP</label>
-          <div
-            className=""
-            style={{
-              display: "flex",
-              gap: "10px",
-              // justifyContent: "center",
-              // alignItems: "center",
-            }}
-          >
-            <div style={{ width: "70%" }}>
-              <input
-                name="inpotp"
-                type="text"
-                placeholder="Enter OTP"
-                onChange={handleChange}
-                style={{ width: "80%" }}
-              />
-            </div>
-            <div style={{ width: "30%" }}>
-              <button
-                onClick={verifyOTP}
-                className="otpButton"
-                style={{
-                  width: "100%",
-                  padding: "10px",
-                  marginTop:"10px",
-                  borderRadius: "5px",
-                  color: "#fff",
-                  cursor: "pointer",
-                }}
-              >
-                Vefify OTP
-              </button>
-            </div>
-          </div>
+      <div style={{ textAlign: "center", paddingTop: "20px" }}>
+        <h1>Create a new account</h1>
+      </div>
 
-          {/*  */}
-          {/* <label htmlFor="">enter otp</label>
+      <div className="register">
+        {/* <form > */}
+        <div className="form">
+          <div className="left">
+            {/* <h1>Create a new account</h1> */}
+            <label htmlFor="">Username</label>
+            <input
+              name="username"
+              type="text"
+              placeholder="johndoe"
+              onChange={handleChange}
+            />
+            <label htmlFor="">Email </label>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                gap: "10px",
+                // justifyContent: "center",
+                // alignItems: "center",
+              }}
+            >
+              <div style={{ width: "70%" }}>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  style={{ width: "80%" }}
+                />
+              </div>
+              <div style={{ width: "30%" }}>
+                <button
+                  onClick={generateOTP}
+                  className="otpButton"
+                  style={{
+                    width: "100%",
+                    padding: "10px",
+                    borderRadius: "5px",
+                    marginTop: "10px",
+                    color: "#fff",
+                    cursor: "pointer",
+                  }}
+                >
+                  Send OTP
+                </button>
+              </div>
+            </div>
+            {/*  */}
+            <label htmlFor="">Enter OTP</label>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                gap: "10px",
+                // justifyContent: "center",
+                // alignItems: "center",
+              }}
+            >
+              <div style={{ width: "70%" }}>
+                <input
+                  name="inpotp"
+                  type="text"
+                  placeholder="Enter OTP"
+                  onChange={handleChange}
+                  style={{ width: "80%" }}
+                />
+              </div>
+              <div style={{ width: "30%" }}>
+                <button
+                  onClick={verifyOTP}
+                  className="otpButton"
+                  style={{
+                    width: "100%",
+                    padding: "10px",
+                    marginTop: "10px",
+                    borderRadius: "5px",
+                    color: "#fff",
+                    cursor: "pointer",
+                  }}
+                >
+                  Vefify OTP
+                </button>
+              </div>
+            </div>
+
+            {/*  */}
+            {/* <label htmlFor="">enter otp</label>
           <input name="otp" type="text" onChange={handleChange} /> */}
-          <label htmlFor="">Password</label>
-          <input name="password" placeholder="*****" type="password" onChange={handleChange} />
-          <label htmlFor="">Profile Picture</label>
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-          {/* <button type="submit">Register</button> */}
-        </div>
-        <div className="right">
-          {/* <h1>I want to become a seller</h1> */}
-          <div className="toggle">
-            <label htmlFor="">want to become a seller</label>
-            <label className="switch">
-              <input type="checkbox" onChange={handleSeller} />
-              <span className="slider round"></span>
-            </label>
+            <label htmlFor="">Password</label>
+            <input
+              name="password"
+              placeholder="*****"
+              type="password"
+              onChange={handleChange}
+            />
+            <label htmlFor="">Profile Picture</label>
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+            {/* <button type="submit">Register</button> */}
           </div>
-          <label htmlFor="">Country</label>
-          <input
-            name="country"
-            type="text"
-            placeholder="Usa"
-            onChange={handleChange}
-          />
-          <label htmlFor="">Phone Number</label>
-          <input
-            name="phone"
-            type="text"
-            placeholder="+1 234 567 89"
-            onChange={handleChange}
-          />
-          <label htmlFor="">Description</label>
-          <textarea
-            placeholder="A short description of yourself"
-            name="desc"
-            id=""
-            cols="30"
-            rows="10"
-            onChange={handleChange}
-          ></textarea>
+          <div className="right">
+            {/* <h1>I want to become a seller</h1> */}
+            <div className="toggle">
+              <label htmlFor="">want to become a seller</label>
+              <label className="switch">
+                <input type="checkbox" onChange={handleSeller} />
+                <span className="slider round"></span>
+              </label>
+            </div>
+            <label htmlFor="">Country</label>
+            <input
+              name="country"
+              type="text"
+              placeholder="Usa"
+              onChange={handleChange}
+            />
+            <label htmlFor="">Phone Number</label>
+            <input
+              name="phone"
+              type="text"
+              placeholder="+1 234 567 89"
+              onChange={handleChange}
+            />
+            <label htmlFor="">Description</label>
+            <textarea
+              placeholder="A short description of yourself"
+              name="desc"
+              id=""
+              cols="30"
+              rows="10"
+              onChange={handleChange}
+            ></textarea>
+          </div>
         </div>
-      </form>
-    </div>
-    <div style={{textAlign:"center", paddingBottom:"20px"}}>
-    <button className="submitbutton"type="submit">Register</button>
-        <div style={{textAlign:"center"}}><Link to="/Login" ><h4>Already have an account click here</h4></Link></div>
-    </div>
+        {/* </form> */}
+      </div>
+
+      <div style={{ textAlign: "center", paddingBottom: "20px" }}>
+        <button
+          onClick={registervalidation}
+          className="submitbutton"
+          type="submit"
+        >
+          Register
+        </button>
+        <div style={{ textAlign: "center" }}>
+          <Link to="/Login">
+            <h4>Already have an account click here</h4>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
