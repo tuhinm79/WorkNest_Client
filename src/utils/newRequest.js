7c56b0e1feb98c1e83ebd82d7518f57a752e570a
tuhinm79 <tuhinmohapatra79@gmail.com>
2024-08-20 21:12:16 +0530

    changing backend url

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const newRequest = axios.create({
-  baseURL: "http://localhost:8888/api",
-  withCredentials: true,
-});
 // const newRequest = axios.create({
-//   baseURL: "https://worknest-backend.onrender.com/api",
+//   baseURL: "http://localhost:8888/api",
 //   withCredentials: true,
 // });
+const newRequest = axios.create({
+  baseURL: "https://worknest-backend.onrender.com/api",
+  withCredentials: true,
+});
 
 export default newRequest;
